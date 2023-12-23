d8c746846ab572daa02bb2a29c10d57daed6ff88
汐知 <xizhi.cx@alibaba-inc.com>
2023-12-23 18:43:42 +0800

    update

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import os
 import sys
-#sys.path.append('.')
-#os.system("pip install gradio==3.50.2")
 import cv2
 import einops
 import numpy as np
 import torch
 import random
 import gradio as gr
-#print(gr.__version__)
-
 import albumentations as A
 from PIL import Image
 import torchvision.transforms as T
@@ -19,6 +15,7 @@ from cldm.ddim_hacked import DDIMSampler
 from omegaconf import OmegaConf
 from cldm.hack import disable_verbosity, enable_sliced_attention
 from huggingface_hub import snapshot_download
+
 
 snapshot_download(repo_id="xichenhku/AnyDoor_models", local_dir="./AnyDoor_models")
 
@@ -35,14 +32,20 @@ if save_memory:
 config = OmegaConf.load('./configs/demo.yaml')
 model_ckpt =  config.pretrained_model
 model_config = config.config_file
-
-
+use_interactive_seg = config.config_file
 
 
 model = create_model(model_config ).cpu()
 model.load_state_dict(load_state_dict(model_ckpt, location='cuda'))
 model = model.cuda()
 ddim_sampler = DDIMSampler(model)
+
+if use_interactive_seg:
+    from iseg.coarse_mask_refine_util import BaselineModel
+    model_path = './iseg/coarse_mask_refine.pth'
+    iseg_model = BaselineModel().eval()
+    weights = torch.load(model_path , map_location='cpu')['state_dict']
+    iseg_model.load_state_dict(weights, strict= True)
 
 
 def crop_back( pred, tar_image,  extra_sizes, tar_box_yyxx_crop):
@@ -222,6 +225,13 @@ ref_list.sort()
 image_list=[os.path.join(image_dir,file) for file in os.listdir(image_dir) if '.jpg' in file or '.png' in file or '.jpeg' in file]
 image_list.sort()
 
+def process_image_mask(image_np, mask_np):
+    img = torch.from_numpy(image_np.transpose((2, 0, 1)))
+    img_ten = img.float().div(255).unsqueeze(0)
+    mask_ten = torch.from_numpy(mask_np).float().unsqueeze(0).unsqueeze(0)
+    return img_ten, mask_ten
+
+
 def mask_image(image, mask):
     blanc = np.ones_like(image) * 255
     mask = np.stack([mask,mask,mask],-1) / 255
@@ -242,6 +252,11 @@ def run_local(base,
     ref_mask = np.asarray(ref_mask)
     ref_mask = np.where(ref_mask > 128, 1, 0).astype(np.uint8)
 
+    # refine the user annotated coarse mask
+    if use_interactive_seg:
+        img_ten, mask_ten = process_image_mask(ref_image, ref_mask)
+        ref_mask = iseg_model(img_ten, mask_ten)['instances'][0,0].detach().numpy() > 0.5
+
     processed_item = process_pairs(ref_image.copy(), ref_mask.copy(), image.copy(), mask.copy(), max_ratio = 0.8)
     masked_ref = (processed_item['ref']*255)
 
@@ -254,24 +269,19 @@ def run_local(base,
     masked_ref = cv2.resize(masked_ref.astype(np.uint8), (512,512))
     return [synthesis]
 
-
-
 with gr.Blocks() as demo:
     with gr.Column():
         gr.Markdown("#  Play with AnyDoor to Teleport your Target Objects! ")
         with gr.Row():
             baseline_gallery = gr.Gallery(label='Output', show_label=True, elem_id="gallery", columns=1, height=768)
             with gr.Accordion("Advanced Option", open=True):
-                num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
+                #num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
                 strength = gr.Slider(label="Control Strength", minimum=0.0, maximum=2.0, value=1.0, step=0.01)
                 ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=30, step=1)
                 scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=5.0, step=0.1)
                 seed = gr.Slider(label="Seed", minimum=-1, maximum=999999999, step=1, value=-1)
                 gr.Markdown(" Higher guidance-scale makes higher fidelity, while lower guidance-scale leads to more harmonized blending.")
     
-
-
-
 
         gr.Markdown("# Upload / Select Images for the Background (left) and Reference Object (right)")
         gr.Markdown("### Your could draw coarse masks on the background to indicate the desired location and shape.")
